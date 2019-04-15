8548836fca881e41bef5ccfd07866ffd4d9f8194
librevlad <librevlad@gmail.com>
2019-04-15 03:57:03 +0300

    wip

--- src/UserLimitsClass.php
+++ src/UserLimitsClass.php
@@ -30,7 +30,7 @@ class UserLimitsClass
         $this->user = $user;
         $this->subject = $subject;
 
-        $this->limit = Limit::where( 'subject', $subject )->where( 'package', $user->resolvePackage() )->first();
+        $this->limit = Limit::where( 'subject', $subject )->where( 'package', $user->resolvePackage() )->firstOrFail();
         $this->usage = $this->limit->usages()->firstOrCreate( [
             'user_id' => $this->user->id,
         ] );
